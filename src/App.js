0cfc1f1c610dbacb6b981cab4cea69eac9e22170
Alan Bedoya <alanjbedoya@gmail.com>
2021-06-13 21:41:18 -0700

    Able to sign in with google, twitter, or github.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Login } from './components';
+import { AuthProvider } from './contexts/authContext';
+import { Login, Chats } from './components';
 
 function App() {
   return (
     <div className='App h-screen bg-gradient-to-r from-pink-400 to-red-400'>
       <Router>
-        <Switch>
-          <Route path='/' component={Login} />
-        </Switch>
+        <AuthProvider>
+          <Switch>
+            <Route exact path='/'>
+              <Login />
+            </Route>
+            <Route path='/chats'>
+              <Chats />
+            </Route>
+          </Switch>
+        </AuthProvider>
       </Router>
     </div>
   );
--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,12 @@
-import { AiFillGoogleCircle, AiFillGithub, AiFillApple } from 'react-icons/ai';
+import {
+  AiFillGoogleCircle,
+  AiFillGithub,
+  AiFillTwitterCircle,
+} from 'react-icons/ai';
 import chatter from '../assets/img/chatter.svg';
+import 'firebase/auth';
+import firebase from 'firebase/app';
+import { auth } from '../firebase';
 
 export const Login = () => {
   return (
@@ -14,55 +21,76 @@ export const Login = () => {
           Welcome to Chatter!
         </h2>
         <form className='flex flex-col pt-3 pb-12 px-4 sm:px-6 lg:px-8'>
-          <div class='w-full md:w-full px-3 mb-6'>
+          <div className='w-full md:w-full px-3 mb-6'>
             <label
-              class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-              for='Password'
+              className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+              htmlFor='Password'
             >
               Email address
             </label>
             <input
-              class='appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none'
+              className='appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none'
               type='email'
               required
             />
           </div>
-          <div class='w-full md:w-full px-3 mb-6'>
+          <div className='w-full md:w-full px-3 mb-6'>
             <label
-              class='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-              for='Password'
+              className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
+              htmlFor='Password'
             >
               Password
             </label>
             <input
-              class='appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none'
+              className='appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none'
               type='password'
               required
             />
           </div>
-          <div class='w-full md:w-full px-3 mb-6'>
-            <button class='appearance-none block w-full bg-blue-600 text-gray-100 font-bold rounded-lg py-3 px-3 leading-tight hover:bg-blue-500 focus:outline-none focus:bg-white focus:border-gray-500'>
+          <div className='w-full md:w-full px-3 mb-6'>
+            <button className='appearance-none block w-full bg-blue-600 text-gray-100 font-bold rounded-lg py-3 px-3 leading-tight hover:bg-blue-500 focus:outline-none focus:bg-white focus:border-gray-500'>
               Sign in
             </button>
           </div>
-          <div class='mx-auto -mb-6 pb-1'>
-            <span class='text-center text-xs text-gray-700'>
+          <div className='mx-auto -mb-6 pb-1'>
+            <span className='text-center text-xs text-gray-700'>
               or sign up with
             </span>
           </div>
           <div className='flex items-center w-full mt-2'>
-            <div class='w-full md:w-1/3 px-3 pt-4 mx-2 border-t border-gray-400'>
-              <button class='appearance-none flex items-center justify-center w-full bg-gray-100 text-gray-700 shadow border border-gray-400 rounded-lg py-3 px-3 leading-tight hover:bg-gray-200 hover:text-gray-700 focus:outline-none'>
+            <div className='w-full md:w-1/3 px-3 pt-4 mx-2 border-t border-gray-400'>
+              <button
+                onClick={() =>
+                  auth.signInWithRedirect(
+                    new firebase.auth.GoogleAuthProvider()
+                  )
+                }
+                className='appearance-none flex items-center justify-center w-full bg-gray-100 text-gray-700 shadow border border-gray-400 rounded-lg py-3 px-3 leading-tight hover:bg-gray-200 hover:text-gray-700 focus:outline-none'
+              >
                 <AiFillGoogleCircle className='h-6 w-6 fill-current text-gray-700' />
               </button>
             </div>
-            <div class='w-full md:w-1/3 px-3 pt-4 mx-2'>
-              <button class='appearance-none flex items-center justify-center w-full bg-gray-100 text-gray-700 shadow border border-gray-400 rounded-lg py-3 px-3 leading-tight hover:bg-gray-200 hover:text-gray-700 focus:outline-none'>
-                <AiFillApple className='h-6 w-6 fill-current text-gray-700' />
+            <div className='w-full md:w-1/3 px-3 pt-4 mx-2'>
+              <button
+                onClick={() =>
+                  auth.signInWithRedirect(
+                    new firebase.auth.TwitterAuthProvider()
+                  )
+                }
+                className='appearance-none flex items-center justify-center w-full bg-gray-100 text-gray-700 shadow border border-gray-400 rounded-lg py-3 px-3 leading-tight hover:bg-gray-200 hover:text-gray-700 focus:outline-none'
+              >
+                <AiFillTwitterCircle className='h-6 w-6 fill-current text-gray-700' />
               </button>
             </div>
-            <div class='w-full md:w-1/3 px-3 pt-4 mx-2 border-t border-gray-400'>
-              <button class='appearance-none flex items-center justify-center w-full bg-gray-100 text-gray-700 shadow border border-gray-400 rounded-lg py-3 px-3 leading-tight hover:bg-gray-200 hover:text-gray-700 focus:outline-none'>
+            <div className='w-full md:w-1/3 px-3 pt-4 mx-2 border-t border-gray-400'>
+              <button
+                onClick={() =>
+                  auth.signInWithRedirect(
+                    new firebase.auth.GithubAuthProvider()
+                  )
+                }
+                className='appearance-none flex items-center justify-center w-full bg-gray-100 text-gray-700 shadow border border-gray-400 rounded-lg py-3 px-3 leading-tight hover:bg-gray-200 hover:text-gray-700 focus:outline-none'
+              >
                 <AiFillGithub className='h-6 w-6 fill-current text-gray-700' />
               </button>
             </div>
